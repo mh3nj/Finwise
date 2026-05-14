3811164495b77a8656e934f5c7ea90b64056aa5e
Mohsen Jafari <anoushzadparsi@gmail.com>
2026-05-14 17:03:00 +0000

    Create config.ts

---
+++ src/i18n/config.ts
@@ -0,0 +1,30 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+import de from "./locales/de.json";
+import en from "./locales/en.json";
+import fa from "./locales/fa.json";
+
+const resources = {
+  en: { translation: en },
+  fa: { translation: fa },
+  de: { translation: de },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+  });
+
+export default i18n;
